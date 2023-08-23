93b3b9cfaec3a3398b1d53921a091f6197c9dafc
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2023-08-23 17:15:00 +0200

    display html description output in app

--- Main.py
+++ Main.py
@@ -13,25 +13,38 @@ if file is not None:
     # define family
     families = ['evolution', 'ranking', 'distribution', 'general', 'correlation', 'partOfAWhole', 'flow']
     family = st.selectbox("family", families)
+    st.write(family)
+    space(1)
 
     # define chartType
     chart_types = get_chart_types(family)
     chartType = st.selectbox("chartType", chart_types)
+    st.write(chartType)
+    space(1)
 
     # define title
     title = st.text_input("title", "Title")
+    st.write(title)
+    space(1)
 
     # define description
     description = st.text_input("description", "Description")
+    st.markdown(description, unsafe_allow_html=True)
+    space(1)
 
     # define keywords
     keywords = st.text_input("keywords", "Keywords")
+    st.write(keywords)
+    space(1)
 
     # define seoDescription
     seoDescription = st.text_input("seoDescription", "SEO Description")
+    st.write(seoDescription)
+    space(1)
 
     # define slug using file name
     slug = file.name.split(".")[0]
+    st.write(slug)
 
     # output meta data
     space(2)
