0da3650ac8c7d8d5eaa94e1eb1afe0d892646a2f
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2023-10-01 10:16:12 +0200

    add candlestick chartype

--- family_to_charttype.py
+++ family_to_charttype.py
@@ -5,7 +5,7 @@ def get_chart_types(family):
                    'correlation': ['scatter', 'heatmap', 'correlogram', 'bubble', 'connectedScatter', 'density2d'],
                    'ranking': ['barplot', 'radar', 'wordcloud', 'parallel', 'lollipop', 'circularBarplot'],
                    'partOfAWhole': ['venn', 'treemap', 'donut', 'pie', 'dendogram', 'circularPacking', 'waffle'],
-                   'evolution': ['line', 'area', 'stackedArea', 'stream', 'timeseries'],
+                   'evolution': ['line', 'area', 'stackedArea', 'stream', 'timeseries', 'candlestick'],
                    'map': ['map', 'hebin', 'cartogram', 'connection', 'bubbleMap'],
                    'flow': ['chordDiagram', 'network', 'sankey', 'arc', 'edgeBundling'],
                    'general': ['colors', 'plotly', 'animation', 'cheatSheets', 'caveats', '3d', 'statistics']
